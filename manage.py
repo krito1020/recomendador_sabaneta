f19d9b0a77f7f80e22d7b1717d8c8f4001d85134
krito1020 <carolina.ospina152@gmail.com>
2025-07-18 16:59:59 -0500

    .

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     BASE_DIR = Path(__file__).resolve().parent
     sys.path.append(str(BASE_DIR))
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
