9823fe3da40d0f704b19303116eb50cbf33a844e
krito1020 <carolina.ospina152@gmail.com>
2025-07-18 20:21:54 -0500

    actualizacion data

--- apps/recomendador/urls.py
+++ apps/recomendador/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='inicio'),
+    path('', views.index, name='recomendador_home'),
     path('registro/', views.registrar_comercio, name='registro'),
 ]
